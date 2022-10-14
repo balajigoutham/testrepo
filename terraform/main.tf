992e7839c05df2986a0c81397d6bfd9a1210ae8d
balajigoutham <102151487+balajigoutham@users.noreply.github.com>
2022-10-14 18:52:45 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,11 +6,43 @@ terraform {
     }
   }
 
-  required_version = ">=0.14.9"
+  required_version = ">=0.14.9" 
+
+   backend "s3" {
+       bucket = "[terrabucket1]"
+       key    = "[arn:aws:kms:ap-south-1:593587691751:alias/aws/s3]"
+       region = "ap-south-1"
+   }
+}
 
 }
 
 provider "aws" {
   version = "~>3.0"
-  region  = "east-us-1"
+  region  = "ap-south-1"
+}
+resource "aws_s3_bucket" "s3Bucket" {
+     bucket = "[terrabucket1]"
+     acl       = "public-read"
+
+     policy  = <<EOF
+{
+     "id" : "MakePublic",
+   "version" : "2012-10-17",
+   "statement" : [
+      {
+         "action" : [
+             "s3:GetObject"
+          ],
+         "effect" : "Allow",
+         "resource" : "arn:aws:s3:::[terrabucket1]/*",
+         "principal" : "*"
+      }
+    ]
+  }
+EOF
+
+   website {
+       index_document = "index.html"
+   }
 }
