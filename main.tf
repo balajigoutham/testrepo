5ef29bca5d978b65b47a1c6f47427c78aaf3ffb9
balajigoutham <102151487+balajigoutham@users.noreply.github.com>
2022-10-14 18:07:17 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,19 +1,8 @@
-# terraform apply -var-file="app.tfvars" -var="createdBy=e2esa"
-
-locals {
-  tags = {
-    Project     = var.project
-    CreatedBy   = var.createdBy
-    CreatedOn   = timestamp()
-    Environment = terraform.workspace
+resource "aws_ecs_cluster" "cluster" {
+  name = "${var.project}-${var.environment}"
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
   }
-}
-
-module "ec2" {
-  #source            = "../../modules/e2esa-module-aws-ec2"
-  source = "git::https://github.com/e2eSolutionArchitect/terraform.git//providers/aws/modules/e2esa-module-aws-ec2?ref=main"
-  #for_each          = toset(var.ec2_names) # toset(["ec21","ec22"])
-  ami               = var.ami
-  availability_zone = var.availability_zone
-  tags              = local.tags
+  capacity_providers = ["FARGATE", ]
 }
