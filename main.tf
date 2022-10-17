268670bd4721516960f1d34958648c41bb82d1df
Balaji Goutham <102151487+balajigoutham@users.noreply.github.com>
2022-10-17 18:26:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
    # Terraform version at the time of writing this post
-  required_version = ">> 0.13"
+  required_version = ">= 0.13.0"
    backend "s3" {
     bucket = "cloudquickpocsbackendtf"
     key = "quickcloudpocsbackend.tfstate"
